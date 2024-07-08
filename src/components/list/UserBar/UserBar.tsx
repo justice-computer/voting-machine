f64b155d59a802f2ca146966ed6691eb19c09f7b
Peter Banka <psbanka@users.noreply.github.com>
2024-07-07 23:02:24 -0700

    🎨 Reorganize buttons and implement logout()

--- src/components/list/UserBar/UserBar.tsx
+++ src/components/list/UserBar/UserBar.tsx
@@ -1,5 +1,7 @@
 import "./userBar.css"
 
+import { useUserStore } from "../../../lib/userStore"
+
 type UserBarProps = {
 	turnOnAdminMode: () => void
 }
@@ -7,12 +9,33 @@ type UserBarProps = {
 function UserBar({ turnOnAdminMode }: UserBarProps): JSX.Element {
 	const { currentUser, logout } = useUserStore()
 
+	function handleLogout() {
+		logout()
+	}
 
 	function handleAdmin() {
 		turnOnAdminMode()
 	}
 
 	return (
+		<div className="UserBar">
+			<div className="user">
+				<img src={currentUser?.avatar ?? `./avatar.png`} alt="avatar" />
+				<h2>{currentUser?.username}</h2>
+			</div>
+			<div className="icons">
+				<button type="button" onClick={handleLogout}>
+					<img src="./power-off-icon.svg" alt="logout" />
+				</button>
+				{currentUser?.admin && (
+					<button type="button" onClick={handleAdmin}>
+						<img src="./gear-icon.svg" alt="admin" />
+					</button>
+				)}
+				<img src="./edit.png" alt="edit" />
+			</div>
+		</div>
+	)
 }
 
 export default UserBar
