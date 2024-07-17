1233cbba4b258ab6e02c2542d05b671827fdb847
Peter Banka <psbanka@users.noreply.github.com>
2024-07-17 14:58:04 -0700

    ✨ Convert to grid-view
    FIXME: clickable images suck

--- src/components/CandidateDetail/CandidateDetail.tsx
+++ src/components/CandidateDetail/CandidateDetail.tsx
@@ -4,10 +4,9 @@ import type { Candidate } from "../../types"
 
 type CandidateDetailProps = {
 	candidate: Candidate | null
-	handleVote: (vote: number | null) => void
 }
 
-function CandidateDetail({ candidate, handleVote }: CandidateDetailProps): JSX.Element | null {
+function CandidateDetail({ candidate }: CandidateDetailProps): JSX.Element | null {
 	if (candidate == null) return null
 	return (
 		<div className="candidateDetail">
@@ -16,40 +15,6 @@ function CandidateDetail({ candidate, handleVote }: CandidateDetailProps): JSX.E
 				<h2>{candidate.name}</h2>
 				<h3>{candidate.heading}</h3>
 				<p>{candidate.details}</p>
-			</div>
-			<div className="icons">
-				<button
-					type="button"
-					onClick={() => {
-						handleVote(1)
-					}}
-				>
-					<img className="first" src="./one-icon.svg" alt="one" />
-				</button>
-				<button
-					type="button"
-					onClick={() => {
-						handleVote(2)
-					}}
-				>
-					<img className="second" src="./two-icon.svg" alt="two" />
-				</button>
-				<button
-					type="button"
-					onClick={() => {
-						handleVote(3)
-					}}
-				>
-					<img className="third" src="./three-icon.svg" alt="three" />
-				</button>
-				<button
-					type="button"
-					onClick={() => {
-						handleVote(null)
-					}}
-				>
-					<img className="cancel" src="./cancel-icon.svg" alt="cancel" />
-				</button>
 			</div>
 		</div>
 	)
