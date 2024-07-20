9976acd1496932f9c50b6d82a8531b1edcb90e01
Jeremy Banka <hello@jeremybanka.com>
2024-07-20 02:21:46 -0700

    💄 finish styling

--- src/stories/Ballot.stories.tsx
+++ src/stories/Ballot.stories.tsx
@@ -1,19 +1,19 @@
 /* eslint-disable quotes */
 import type { Meta, StoryObj } from "@storybook/react"
 
-import { Ballot } from "./Ballot"
+import { BallotSheet } from "./Ballot"
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
 	title: "Example/Ballot",
-	component: Ballot,
+	component: BallotSheet,
 	parameters: {
 		// Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
 		layout: "centered",
 	},
 	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
 	tags: ["autodocs"],
-} satisfies Meta<typeof Ballot>
+} satisfies Meta<typeof BallotSheet>
 
 export default meta
 type Story = StoryObj<typeof meta>
@@ -41,7 +41,7 @@ export const Primary: Story = {
 					},
 				],
 				config: {
-					numberOfWinners: 3n,
+					numberOfWinners: 1n,
 					votingTiers: [1n, 1n, 1n, 1n, 1n, 1n],
 				},
 			},
