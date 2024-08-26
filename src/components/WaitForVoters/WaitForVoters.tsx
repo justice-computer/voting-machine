e6c5db8416a2819fd2bdc0d571af7d37bb59848e
Peter Banka <psbanka@users.noreply.github.com>
2024-08-26 08:52:37 -0700

    ✨ Got graphing working

--- src/components/WaitForVoters/WaitForVoters.tsx
+++ src/components/WaitForVoters/WaitForVoters.tsx
@@ -25,6 +25,7 @@ function getLiveCandidateVotes(currentElectionVoters: CurrentElectionVoters[]): 
 	const candidates = useO(candidatesInCurrentElectionSelector).filter(
 		(candidate) => candidate.id !== undefined,
 	)
+	if (candidates.length === 0) return {}
 	const output: LiveCandidateVotes = candidates.reduce((acc: LiveCandidateVotes, candidate) => {
 		// FIXME: hard-coded to 6 tiers
 		acc[candidate.id] = Array(6).fill(0)
@@ -46,16 +47,24 @@ function getLiveCandidateVotes(currentElectionVoters: CurrentElectionVoters[]): 
 }
 
 function transformToGraphable(liveCandidateVotes: LiveCandidateVotes): GraphableCandidateVote[] {
+	const candidates = useO(candidatesInCurrentElectionSelector).filter(
+		(candidate) => candidate.id !== undefined,
+	)
 	const output: GraphableCandidateVote[] = Object.entries(liveCandidateVotes).map(
-		([candidateId, tierTotals]) => ({
-			id: candidateId,
-			t1: tierTotals[0],
-			t2: tierTotals[1],
-			t3: tierTotals[2],
-			t4: tierTotals[3],
-			t5: tierTotals[4],
-			t6: tierTotals[5],
-		}),
+		([candidateId, tierTotals]) => {
+			const candidate = candidates.find((c) => c.id === candidateId)
+			if (!candidate?.name) throw new Error(`Candidate "${candidateId}" has no name`)
+			return {
+				// id: candidateId,
+				name: candidate.name,
+				t1: tierTotals[0],
+				t2: tierTotals[1],
+				t3: tierTotals[2],
+				t4: tierTotals[3],
+				t5: tierTotals[4],
+				t6: tierTotals[5],
+			}
+		},
 	)
 	return output
 }
@@ -66,22 +75,20 @@ function WaitForVoters({ targetState }: WaitForVotersProps): JSX.Element {
 
 	const liveCandidateVotes = getLiveCandidateVotes(currentElectionVoters)
 	const graphableCandidateVotes = transformToGraphable(liveCandidateVotes)
-	console.log(JSON.stringify(liveCandidateVotes, null, 2))
+	console.log(JSON.stringify(graphableCandidateVotes, null, 2))
 	return (
 		<div className="waitForVoters">
 			{currentElectionVoters.length ? (
-				<div className="waiting">
-					<h1>Waiting for voters...</h1>
-					<h2 style={{ padding: `10px` }}>Current voters:</h2>
+				<>
 					<ul>
 						<BarChart
 							data={graphableCandidateVotes}
-							width={500}
-							height={300}
-							margin={{ top: 20, left: 50, right: 40, bottom: 100 }}
+							width={320}
+							height={700}
+							margin={{ top: 20, left: 150, right: 40, bottom: 100 }}
 						/>
 					</ul>
-				</div>
+				</>
 			) : (
 				<div>
 					<h1>Waiting for voters...</h1>
