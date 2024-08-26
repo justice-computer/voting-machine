5ce3251f83dd8f75c1ea5938a9d5445ee4f25ace
Peter Banka <psbanka@users.noreply.github.com>
2024-08-25 21:06:03 -0700

    🚧 WIP Working graph

--- src/types.tsx
+++ src/types.tsx
@@ -35,9 +35,25 @@ export type ElectionData = {
 	subtitle: string
 }
 
+export interface GraphableCandidateVote {
+	name?: string
+	id: string
+	t1: number
+	t2: number
+	t3: number
+	t4: number
+	t5: number
+	t6: number
+}
+
 export type ActualVote = {
 	voterId: string
 	electionId: string
+	// 0th index is the first vote-tier.
+	// For elections with one vote per tier
+	// like Portland, each entry will be an array
+	// of one candidate string.
+	// -@peba Aug 2024
 	tierList: string[][]
 	finished: boolean
 }
